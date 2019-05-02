2f8e9fa2fb103030a0744131a66592a7d0ff16b2
ivan <ivan@rocketfirm.com>
2019-05-02 17:34:54 +0600

    migrations, entities, repositories

---
+++ module/Tournament/src/Entity/Game.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ivan
+ * Date: 02.05.19
+ * Time: 17:32
+ */
+
+namespace Tournament\Entity;
+
+/**
+ * This class represents a game entity in a tournament.
+ * @ORM\Entity(repositoryClass="\Tournament\Repository\GameRepository")
+ * @ORM\Table(name="tournament")
+ */
+class Game
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(name="id")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(name="title")
+     */
+    protected $title;
+
+    /**
+     * @ORM\Column(name="created_at")
+     */
+    protected $created_at;
+
+    /**
+     * Returns ID of this team.
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Sets ID of this team.
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * Returns title.
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Sets title.
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * Returns the date when this team was created.
+     * @return string
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Sets the date when this team was created.
+     * @param string $created_at
+     */
+    public function setCreatedAt($created_at)
+    {
+        $this->created_at = $created_at;
+    }
+}
